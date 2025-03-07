00efd565299872eacb518fdf1e9f690d9cf71ae3
ASalinasVe <alejandra.salinas@ucb.edu.bo>
2025-03-07 19:17:59 -0400

    Codigo limpio y completo

--- src/calcRomano.js
+++ src/calcRomano.js
@@ -1,26 +1,8 @@
-export function calcRomano(numero) {
-
-    //500, 400, 100, 90, 50,
-    //"D", "CD", "C", "XC", "L", 
+export function calcRomano(numero) { 
 
     let valores = [ 500, 400, 100, 90, 50,40, 10, 9, 5, 4, 1]
     let romanos = ["D","CD", "C", "XC", "L","XL", "X", "IX", "V", "IV", "I"];
-;
-    // if (numero == 1){
-    //     return "I";
-    // }
-    // else if (numero == 4){
-    //     return "IV";
-    // }
-    // else if (numero == 5){
-    //     return "V";
-    // }
-    // else if (numero == 9){
-    //     return "IX";
-    // }
-    // else if (numero == 10){
-    //     return "X";
-    // }
+
     
     let resultado = "";
 
@@ -31,7 +13,9 @@ export function calcRomano(numero) {
             numero -= valores[i];
         }
     }  
+
     return resultado;    
+    
 };
 
 
